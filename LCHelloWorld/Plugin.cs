24cf84a995314161a3f045597426b2e37294739f
Redko Denys <63252297+um3neko@users.noreply.github.com>
2024-12-13 21:19:01 +0200

    Add project files.

---
+++ LCHelloWorld/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCHelloWorld
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class HelloWorldPlugin : BaseUnityPlugin
+    {
+        private const string GUID = "23452f0b-701c-421d-a33b-4245e9657b80";
+        private const string Name = "Hello World";
+        private const string Version = "0.0.1";
+
+        private readonly Harmony Harmony = new Harmony(GUID);
+
+        private static HelloWorldPlugin Instance;
+
+        void Awake()
+        {
+            if(Instance == null)
+                Instance = this;
+
+            Harmony.PatchAll(typeof(HelloWorldPlugin));
+        }
+
+    }
+}
